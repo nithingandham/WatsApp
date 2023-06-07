e1c36bb1771908b844d223f7499026addba296f6
ashishchillale556 <ashishchillale556@gmail.com>
2023-06-07 16:15:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+  function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>new watsap</h1>
+        <p>
+          sasthri
+        </p>
+        <a
+          className="App-link"
+          href="https://web.whatsapp.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         click for scanner
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
